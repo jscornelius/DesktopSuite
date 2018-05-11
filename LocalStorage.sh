2829b63be3a187984d617609e939015f8226cad8
jcornelius <john@nonfatmedia.com>
2018-05-11 10:43:44 -0700

    added script to clean local storage out

---
+++ LocalStorage.sh
@@ -0,0 +1,4 @@
+rm -rf ~/Library/Cache/DesktopSuite
+rm -rf ~/Library/Saved\ Application\ State/DesktopSuite
+rm -rf ~/Library/Application\ Support/DesktopSuite
+rm -rf ~/Library/Application Support/Electron/Local\ Storage
