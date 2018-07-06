a8c6ac1a53c89ce6e3e5e0b55974131864401e1d
jscornelius <johnscornelius@gmail.com>
2018-07-06 10:14:22 -0700

    build script cleanup

--- buildscripts/winbuild.bat
+++ buildscripts/winbuild.bat
@@ -1,1 +1,2 @@
-electron-packager .. DesktopSuite --overwrite --asar=true --platform=win32 --arch=ia32 --icon=../src/assets/icons/win/icon.ico  --out=../release-builds --version-string.CompanyName=BreakdownServices --version-string.FileDescription=DesktopSuite --version-string.ProductName="Breakdown Services Desktop Suite"
+#electron-packager .. DesktopSuite --overwrite --asar=true --platform=win32 --arch=ia32 --icon=../src/assets/icons/win/icon.ico  --out=../release-builds --version-string.CompanyName=BreakdownServices --version-string.FileDescription=DesktopSuite --version-string.ProductName="Breakdown Services Desktop Suite"
+npm run make
