f36e5a8ece3ec49a11f4d8ff16d75713b3d94eb6
jscornelius <john@nonfatmedia.com>
2018-07-12 12:44:45 -0700

    ongoing feeble attempt at automating this build process part 138

--- buildscripts/winbuild.bat
+++ buildscripts/winbuild.bat
@@ -1,1 +1,5 @@
 npm run make
+
+electron-packager . DesktopSuite --platform=win32 --arch=ia32
+
+electron-packager . --platform=win32 --arch=x64 --asar --overwrite --icon="./src/assets/icons/win/icon.ico" --out=./dist
